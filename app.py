25f929fa8d24c844053ef4beb895220f4b09929d
Lenus Walker <walker.lenus@gmail.com>
2018-06-03 20:22:32 -0400

    formatting

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("home.html", title="AGM 2017")
+    return render_template('home.html', title='AGM 2017')
 
 
 if __name__ == '__main__':
